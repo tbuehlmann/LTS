449b875e01d45a7b6fed08d332d72f2e45ad7242
Nicola Hauke <jasinai@jasinai.de>
2014-11-06 21:22:22 +0100

    Forgotten LTS

--- start.rb
+++ start.rb
@@ -11,3 +11,15 @@ light_lts.to_tex("light")
 lightCompSwitch = light_lts.compose(switch_lts, ["press", "hold"])
 lightCompSwitch.to_tex("comp")
 `pdflatex comp.tex`
+
+tl1 = LTS.new
+tl1.parse_json("TL1.json")
+tl1.to_tex("TL1")
+`pdflatex TL1.tex`
+tl2 = LTS.new
+tl2.parse_json("TL2.json")
+tl2.to_tex("TL2")
+`pdflatex TL2.tex`
+TL1u2 = tl1.compose(tl2, [])
+TL1u2.to_tex("TL1u2")
+`pdflatex TL1u2.tex`
